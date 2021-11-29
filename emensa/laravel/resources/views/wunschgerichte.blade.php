54800fe51e860dd69b2b8a1cc5f83c73a39428f7
Luis Diniz Do Carmo <81914129+LuisDoc@users.noreply.github.com>
2021-11-29 12:01:46 +0100

    Wunschgericht.css überarbeitet - An allgemeines Design angepasst

--- emensa/laravel/resources/views/wunschgerichte.blade.php
+++ emensa/laravel/resources/views/wunschgerichte.blade.php
@@ -4,22 +4,16 @@
     <!-- Eingabemöglichkeiten für ein Wunschgericht-->
 
     @foreach ($errors->all() as $error)
-        <?php Alert::error("Fehler",$error)?>;
+        <?php Alert::error('Fehler', $error); ?>;
     @endforeach
-    <form method="POST" action="/sendWunschgericht">
+    <form class="" method="POST" action="/sendWunschgericht">
         @csrf
-        <div id="firstRow">
-            <input class="wunschgericht_smallinput " type="text" name="name" placeholder="name" value="">
-            <input class="wunschgericht_smallinput" type="text" name="email" placeholder="email" value="">
-            <input class="wunschgericht_smallinput" type="text" name="gericht" placeholder="Gerichtname" value="">
+        <div class="wunschgericht_form">
+            <input type="text" name="name" placeholder="name" value="">
+            <input type="text" name="email" placeholder="email" value="">
+            <input type="text" name="gericht" placeholder="Gerichtname" value="">
+            <textarea rows="3" class="wunschgericht_biginput" name="beschreibung" placeholder="Beschreibung"></textarea>
+            <button type="submit" name="submit" class="wunschgericht_submit">Absenden</button>
         </div>
-        <div class="secondRow">
-            <textarea rows="20" class="wunschgericht_biginput" name="beschreibung" placeholder="Beschreibung"></textarea>
-            <button type="submit" name="submit" class="wunschgericht_submit">Speichern</button>
-        </div>
-        <div class="thirdRow">
-        </div>
-
-
     </form>
 @endsection
