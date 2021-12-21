d4f893ad2f5f0c361a2926b78435cec91f4cadbb
Nilusche Liyanaarachchi <73897941+Nilusche@users.noreply.github.com>
2021-12-21 16:44:47 +0100

    Update m5_4.sql

--- beispiele/m5_4.sql
+++ beispiele/m5_4.sql
@@ -2,13 +2,14 @@ create view view_anmeldungen as
 select *
 from benutzer
 Order by (anzahlanmeldungen) DESC;
-create view view_kategoriegerichte_vegetarisch as
-Select DISTINCT kategorie.name as Kategoriename,
-    (gericht.name) as Gerichtname
-from kategorie
-    left join gericht_hat_kategorie on gericht_hat_kategorie.kategorie_id = kategorie.id
-    left join gericht on gericht_hat_kategorie.gericht_id = gericht.id
-    and gericht.vegetarisch = 1;
+create view view_kategoriegerichte_vegetarisch as 
+select gericht.name as Gerichtname, kategorie.name as Kategoriename
+from gericht 
+join gericht_hat_kategorie
+on gericht_hat_kategorie.gericht_id = gericht.id and gericht.vegetarisch =1
+right join kategorie
+on gericht_hat_kategorie.kategorie_id = kategorie.id
+order by gericht.name desc;
 create view view_suppengerichte as
 select *
 from gericht
